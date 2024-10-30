371c2bb38d99a1d1bb74720f774e5ca5fcf33ca4
vald <gpm.pontes@gmail.com>
2024-10-30 21:38:49 +0000

    Added sorting

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const addNewItem = () => {
   li.addEventListener('click', () => {
     li.classList.toggle('done');
   });
+  li.setAttribute('created-at', new Date());
   list.appendChild(li);
 
   const text = document.createElement('div');
@@ -52,9 +53,81 @@ const addNewItem = () => {
   input.value = '';
 };
 
+const sortlist = (sorting) => {
+  console.log(sorting);
+  const list = document.getElementById('list');
+  const items = list.getElementsByTagName('li');
+  var itemsArray = Array.from(items);
+
+  console.log(itemsArray);
+
+  switch (sorting) {
+    case 'newest':
+      sortByDate(itemsArray, 'latest');
+      break;
+    case 'oldest':
+      sortByDate(itemsArray, 'oldest');
+      break;
+    case 'alphanumeric-asc':
+      itemsArray = sortAlphabetically(itemsArray, 'asc');
+      break;
+    case 'alphanumeric-desc':
+      itemsArray = sortAlphabetically(itemsArray, 'desc');
+      break;
+    case 'done':
+      itemsArray = itemsArray.filter((item) => item.classList.contains('done'));
+      break;
+    case 'not-done':
+      itemsArray = itemsArray.filter(
+        (item) => !item.classList.contains('done')
+      );
+      break;
+    default:
+      break;
+  }
+
+  list.innerHTML = '';
+  itemsArray.forEach((item) => {
+    list.appendChild(item);
+  });
+};
+
+const sortByDate = (items, order) => {
+  return items.sort((a, b) => {
+    const dateA = new Date(a.getAttribute('created-at'));
+    const dateB = new Date(b.getAttribute('created-at'));
+
+    if (order === 'latest') {
+      return dateB - dateA;
+    } else {
+      return dateA - dateB;
+    }
+  });
+};
+
+const sortAlphabetically = (items, order) => {
+  return items.sort((a, b) => {
+    const textA = a.getElementsByClassName('todo-text')[0].innerHTML;
+    const textB = b.getElementsByClassName('todo-text')[0].innerHTML;
+
+    if (order === 'asc') {
+      return textA.localeCompare(textB);
+    } else {
+      return textB.localeCompare(textA);
+    }
+  });
+};
+
 const init = () => {
   const button = document.getElementById('add-item-button');
   button.addEventListener('click', addNewItem);
+  document
+    .getElementById('new-item-input')
+    .addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        addNewItem();
+      }
+    });
 };
 
 init();
